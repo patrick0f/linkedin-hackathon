0528b509922860e81905ef76d0e24c8d0936703e
patrick0f <pfung5423@gmail.com>
2025-06-16 21:52:29 -0700

    Fixed backend dotenv bug

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+dotenv.config();
 import connectDB from './lib/db';
 import postsRouter from './routes/posts';
 import usersRouter from './routes/users';
 
-dotenv.config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
